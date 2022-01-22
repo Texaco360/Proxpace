16af759cf409ddaa73563f71fefaa8e2e6956fc1
Texaco360 <decrooskoenraad@gmail.com>
2022-01-22 17:18:24 +0100

    adding extra icons

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+/****HOME****/
+/***START MAIN***/
+/**START HERO**/
+
+
+/**EINDE HERO**/
+/**START REVIEW**/
+
+
+/**EINDE REVIEW**/
+/**START LOCATIES**/
+
+
+/**EINDE LOCATIES**/
+/***EINDE MAIN***/
